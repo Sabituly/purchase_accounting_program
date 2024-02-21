43f4d71b0494d7a3ce481b871092ac81a65460ac
Daniyar <ichbindaniyar@gmail.com>
2024-02-21 19:03:05 +0600

    Added CostItem Companent

--- purchase_accounting_program/src/App.js
+++ purchase_accounting_program/src/App.js
@@ -1,8 +1,11 @@
+import CostItem from "./companents/CostItem";
 
 function App() {
   return (
     <div>
       <h1>Начнём изучение Реакт!!!</h1>
+      {/*<p>Hey there!</p>*/}
+      <CostItem></CostItem>
     </div>
   );
 }
