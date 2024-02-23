f4163a857e53c86a97f670ea2308daaad251dc29
Daniyar <ichbindaniyar@gmail.com>
2024-02-23 19:12:08 +0600

    Added CostItem.css N modified index.css

--- purchase_accounting_program/src/companents/CostItem.js
+++ purchase_accounting_program/src/companents/CostItem.js
@@ -1,10 +1,16 @@
+import './CostItem.css';
 function CostItem() {
+
+    const costDate = new Date(2024, 2, 23);
+    const costDescription = 'МакбукПро2024';
+    const costAmount = 999.99;
+
   return (
-    <div>
+    <div className='cost-item'>
       <div>Март 2021 12</div>
-      <div>
-        <h2>Холодильник</h2>
-        <div>$999.99</div>
+      <div className='cost-item__description'>
+        <h2>{costDescription}</h2>
+        <div className='cost-item__price'>{costAmount}</div>
       </div>
     </div>
   );
