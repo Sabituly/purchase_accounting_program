9a4ef0179e57c625dee1f080451f30413eb3a1db
Daniyar <ichbindaniyar@gmail.com>
2024-03-04 00:39:23 +0600

    Edited CostItem

--- purchase_accounting_program/src/companents/Costs/CostItem.js
+++ purchase_accounting_program/src/companents/Costs/CostItem.js
@@ -1,25 +1,18 @@
 import "./CostItem.css";
 import CostDate from "./CostDate";
 import Card from "../UI/Card";
-import React, { useState} from "react";
+
 
 function CostItem(props) {
-
-    const [description, setDescription] = useState(props.description);
-    console.log('Text in component definition');
-    const changeDescriptionHandler = () => {
-        setDescription('New Text');
-        console.log(description);
-    };
 
   return (
     <Card className="cost-item">
       <CostDate date={props.date}/>
       <div className="cost-item__description">
-        <h2>{description}</h2>
+        <h2>{props.description}</h2>
         <div className="cost-item__price">${props.amount}</div>
       </div>
-        <button onClick={changeDescriptionHandler}>Изменить описание</button>
+
     </Card>
   );
 }
