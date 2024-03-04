a696458c8aa214d1707ab99fa0cf15c2cea2cd6a
Daniyar <ichbindaniyar@gmail.com>
2024-03-04 15:09:57 +0600

    Added key indetificator

--- purchase_accounting_program/src/companents/Costs/Costs.js
+++ purchase_accounting_program/src/companents/Costs/Costs.js
@@ -18,6 +18,7 @@ const Costs = (props) => {
                 <CostsFilter year={selectedYear} onChangeYear={getDataFromChild}/>
                 {props.costs.map((cost) => (
                     <CostItem
+                        key = {cost.id}
                         date={cost.date}
                         description={cost.description}
                         amount={cost.amount}
